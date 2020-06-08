6cf353aee610dbd67ec6f32c864dd5706aa96fda
sidiki123 <sidikiouedraogo2000@gmail.com>
2020-06-08 18:06:41 +0100

    fichier ignorés

---
+++ storage/framework/views/18a6ccab0ff22581440e3d38d97f7433a235f690.php
@@ -0,0 +1,17 @@
+<hr>
+<div class="foot">
+    <div>
+        <p>©Copyright Simplon-Burkina, Juin 2020.</p>
+    </div>
+
+    <div id="maricon">
+        <img src="assets/img/face.png" href="www.facebook.com" alt="facebook">
+        <img src="assets/img/whatsapp.png" href="www.whatsapp.com" alt="whatsapp">
+        <img src="assets/img/youtube.png" href="www.youtube.com" alt="youtube">
+        <img src="assets/img/git.png" href="www.github.com" alt="github">
+    </div>
+</div>
+
+</body>
+</html>
+<?php /**PATH C:\laragon\www\Simplon-Burkina\resources\views/partials/footer.blade.php ENDPATH**/ ?>
